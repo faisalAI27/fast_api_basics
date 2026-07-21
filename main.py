1de5492d2d9624e52e3d481c1d08af300f1cb5cf
faisalAI27 <u2023189@giki.edu.pk>
2026-07-21 13:03:54 +0500

    path parameters

--- main.py
+++ main.py
@@ -1,16 +1,5 @@
 from fastapi import FastAPI
 app=FastAPI()
-@app.get("/")
-def read_root():
-    return {'hello':'world'}
-@app.post("/items/")
-def create_item(name: str, price: float):
-    return{'name':name, 'price': price}
-
-@app.put("/items/{item_id}")
-def update_item(item_id:int,name:str,price:float):
-    return{'item id':item_id,"name":name,"price":price}
-
-@app.delete("/items/{item_id}")
-def delete_item(item_id:int):
-    return{"message":f"item id: {item_id} deleted successfully"}
+@app.get("/user/{user_id}")
+def read_user(user_id:int):
+    return{"user_id":user_id}
