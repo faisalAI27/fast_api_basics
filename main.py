74e6221fc209cdfbd2d2c8c72a68715645154d34
faisalAI27 <u2023189@giki.edu.pk>
2026-07-21 12:49:14 +0500

    added delete on server

--- main.py
+++ main.py
@@ -6,3 +6,11 @@ def read_root():
 @app.post("/items/")
 def create_item(name: str, price: float):
     return{'name':name, 'price': price}
+
+@app.put("/items/{item_id}")
+def update_item(item_id:int,name:str,price:float):
+    return{'item id':item_id,"name":name,"price":price}
+
+@app.delete("/items/{item_id}")
+def delete_item(item_id:int,name:str,price:float):
+    return{"message":f"item id: {item_id} deleted successfully"}
